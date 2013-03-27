a5a5be4653ff300a541dd9a2dcfbd4756fc44f84
hyunkeln <hyunkeln@gmail.com>
2013-03-27 16:23:32 -0600

    Agregada navegacion horizontal en standards

--- application/controllers/eservicios.js
+++ application/controllers/eservicios.js
@@ -106,7 +106,7 @@ var es = {
 		.done(function(content,tpl){
 			if(empty) $(holder).empty();
 			$.tmpl(tpl[0] , content[0]).appendTo(holder);
-			$(holder).fadeIn();
+		$(holder).fadeIn();
 			if(typeof func!=="undefined") if(func!=null) func();
 		})
 		.fail(function(a,b,c){
@@ -135,20 +135,25 @@ var es = {
 		var floorName = $(".nid"+nid).parents("section").attr("name");
 		var holder  = '.floor.'+clearString(floorName)+" .floorStandards";
 		var width = $(holder).outerWidth();
+		var shouldAnimate = true;
 		if(dir==-1) 
 			$(holder).animate({left:width});
 		else if(dir==1)
 			$(holder).animate({left:-width});		
-		if(nav.isHomeVisible(floorName))
+		if(nav.isHomeVisible(floorName)){
 			nav.floorToStandard(floorName);
+			shouldAnimate = false;
+			console.log("Visible el home");
+		}
 		es.renderServiceTemplateToHolder(cnf.services.node+nid,cnf.views.standardNd,holder,true,function(){
-			$(holder).animate({
-				left:-1*parseInt($(holder).css("left"))},
-				{duration:1,
-				complete:function(){
-					$(holder).animate({left:0});
-				}
-			});
+			if(shouldAnimate) 
+				$(holder).animate({
+					left:-1*parseInt($(holder).css("left"))},
+					{duration:1,
+					complete:function(){
+						$(holder).animate({left:0});
+					}
+				});
 		},dir==0);
 	},
 	loadPremium:function(nid){
@@ -175,11 +180,6 @@ nav = {
 	floorToPremium:function(floorName){
 		nav.floorTo(floorName,".floorPremiums");
 	},
-	toFloorHome:function(obj){
-		var floorName = $(obj).parents("section").attr("name");
-		$('.floor.'+clearString(floorName)).find(".floorContent").fadeOut().addClass("hide");
-		$('.floor.'+clearString(floorName)).find(".floorHome").fadeIn().removeClass("hide");
-	},
 	isHomeVisible:function(floorName){
 		return !$('.floor.'+clearString(floorName)).find(".floorHome").hasClass("hide");
 	},
@@ -204,9 +204,44 @@ nav = {
 		});
 		//console.log(objs);
 	}, 
+	animateInOut:function(holder,holder2,toLeft){
+		if(typeof toLeft==="undefined") toLeft=true;
+		var multiplier = 3;
+		if(toLeft) multiplier*=-1;
+		var bounce = 0;
+		$(holder).animate({
+			left:multiplier*parseInt($(holder).css("width"))},
+			{duration:500,
+			complete:function(){
+				if(bounce==0){ //Como hay 3 objetos con la clase floorContent, hay que impedir el 'rebote'
+					bounce = 1;
+					$(holder).fadeOut().addClass("hide");
+					//var holder2 = $('.floor.'+clearString(floorName)).find(".floorHome");		
+					if(toLeft) $(holder2).animate({left:"-"+$(holder2).css("left").replace("-","").replace("px","")},{duration:1});
+					else $(holder2).animate({left:$(holder2).css("left").replace("-","").replace("px","")},{duration:1});
+					$(holder2).fadeIn().removeClass("hide");
+					$(holder2).animate({
+						left:0},
+						{duration:500,
+						complete:function(){
+							$(holder2).animate({left:0});
+						}
+					});
+				}
+			}
+		});
+	},
+	toFloorHome:function(obj){
+		var floorName = $(obj).parents("section").attr("name");
+		nav.animateInOut($('.floor.'+clearString(floorName)).find(".floorContent"),$('.floor.'+clearString(floorName)).find(".floorHome"));
+		//$('.floor.'+clearString(floorName)).find(".floorContent").fadeOut().addClass("hide");
+		//$('.floor.'+clearString(floorName)).find(".floorHome").fadeIn().removeClass("hide");
+	},
 	floorTo:function(floorName,container){
-		$('.floor.'+clearString(floorName)).find(".floorContent").fadeOut().addClass("hide");
-		$('.floor.'+clearString(floorName)).find(container).fadeIn().removeClass("hide");
+		//var holder = $('.floor.'+clearString(floorName)).find(".floorContent");
+		nav.animateInOut($('.floor.'+clearString(floorName)).find(".floorContent"),$('.floor.'+clearString(floorName)).find(container));
+		//$('.floor.'+clearString(floorName)).find(".floorContent").fadeOut().addClass("hide");
+		//$('.floor.'+clearString(floorName)).find(container).fadeIn().removeClass("hide");
 	},
 	setVisible:function(visible){
 		if(!visible){
